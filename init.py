89f8e2b8c6ef36caa02f986cbafbc7c85bbe9455
Mitchell Zhou <mz2909@nyu.edu>
2023-12-02 17:03:01 -0500

    initial

--- init.py
+++ init.py
@@ -3,15 +3,36 @@ import pymysql.cursors
 from app import app, get_conn
 import string, random
 import login
-
+#
 app.secret_key = 'Q3I3Pm1lc3NpQ3I3Pm1lc3NpQ3I3Pm1lc3Np'
-app.config['SESSION_COOKIE_HTTPONLY'] = True
+# app.config['SESSION_COOKIE_HTTPONLY'] = True
 
 # Index page
+
+
+valid_credentials = {
+    "username": "user",
+    "password": "password"
+}
 @app.route('/')
 def index():
     print(session)
-    return make_response(render_template('index.html'))
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Check if the provided credentials match the valid credentials
+    if username == valid_credentials['username'] and password == valid_credentials['password']:
+        # Successful login, you can redirect the user to a different page
+        return redirect(url_for('index'))
+    else:
+        # Failed login, you can render the login page with an error message
+        return render_template('login.html', error="Invalid credentials")
+
+
 
 
 if __name__ == "__main__":
