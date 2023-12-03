a0e090f74f9a2e7de5f80af729e3347c47a3c078
Mitchell Zhou <mz2909@nyu.edu>
2023-12-02 22:26:14 -0500

    good

--- init.py
+++ init.py
@@ -1,36 +1,54 @@
 from flask import Flask, render_template, request, session, url_for, redirect, make_response
-import pymysql.cursors
 from app import app, get_conn
 import string, random
-import login
 #
 app.secret_key = 'Q3I3Pm1lc3NpQ3I3Pm1lc3NpQ3I3Pm1lc3Np'
 # app.config['SESSION_COOKIE_HTTPONLY'] = True
 
 # Index page
 
+cursor = get_conn().cursor()
 
 valid_credentials = {
     "username": "user",
     "password": "password"
 }
+
+def authenticate(username, password):
+    query = "SELECT * FROM users WHERE username = %s AND password = %s"
+    cursor.execute(query, (username, password))
+
+    user = cursor.fetchone()
+
+    if not user:
+        return False
+    else:
+        name = user['name']
+        session['email'] = user['email']
+        return True
+
+
 @app.route('/')
 def index():
-    print(session)
-    return render_template('index.html')
+    res = make_response(render_template('index.html'))
+    res.set_cookie("session", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiR3Vlc3QifQ.Yj7FTx2j2yC18jB01ovovVdr4yfcDcc0spVhsdkcCNk")
 
-@app.route('/login', methods=['POST'])
+    return res
+
+@app.route('/login', methods=['GET', 'POST'])  # Allow both GET and POST methods
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    # Check if the provided credentials match the valid credentials
-    if username == valid_credentials['username'] and password == valid_credentials['password']:
-        # Successful login, you can redirect the user to a different page
-        return redirect(url_for('index'))
-    else:
-        # Failed login, you can render the login page with an error message
-        return render_template('login.html', error="Invalid credentials")
+        if authenticate(username, password):
+            message = "Sorry, this user is doesn't exist"
+            return render_template('login.html', error=message)
+        else:
+            return render_template('index.html')
+
+    # This part handles the GET request when accessing the login page
+    return render_template('login.html')
 
 
 
